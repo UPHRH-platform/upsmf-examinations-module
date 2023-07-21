c14dfd2cd5948f2fa60eb052987fe6abb9dff478
Jesly_Joseph <jesly.joseph@tarento.com>
2023-07-21 11:41:26 +0530

    Adding exam-cycle

---
+++ src/app/modules/manage-exams-module/components/exam-cycle/exam-cycle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-exam-cycle',
+  templateUrl: './exam-cycle.component.html',
+  styleUrls: ['./exam-cycle.component.scss']
+})
+export class ExamCycleComponent {
+ 
+  
+}
